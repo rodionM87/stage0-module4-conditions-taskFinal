50a4d5182eb5f5edae98e75e573f2369ed8f2154
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-24 11:05:53 +0600

    Update SeasonDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -3,19 +3,19 @@ package school.mjc.stage0.conditions.finalTask;
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
         default -> {
-				yield "Wrong month number";
-			}
-			case 12, 1, 2 -> {
-				yield "Winter";
-			}
-			case 3, 4, 5 -> {
-				yield "Spring";
-			}
-			case 6, 7, 8 -> {
-				yield "Summer";
-			}
-			case 9, 10, 11 -> {
-				yield "Autumn";
-			}
+	    Syetsm.out.println("Wrong month number");
+	}
+	case 12, 1, 2 -> {
+	    Syetsm.out.println("Winter");
+	}
+	case 3, 4, 5 -> {
+	    Syetsm.out.println("Spring");
+	}
+	case 6, 7, 8 -> {
+	    Syetsm.out.println("Summer");
+	}
+	case 9, 10, 11 -> {
+	    Syetsm.out.println("Autumn");
+	}
     }
 }
